3a414e28938e946fb5b1df0cfdbbd57df556b773
Sushant Agrawal <sagarwal@netwoven.com>
2017-11-13 13:52:13 +0530

    init

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {ROUTES} from './app.routes';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppService} from './app.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule, HttpClientModule
     //, RouterModule.forRoot(ROUTES, {useHash: true})
@@ -17,4 +15,4 @@ import {AppService} from './app.service';
   providers: [AppService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
