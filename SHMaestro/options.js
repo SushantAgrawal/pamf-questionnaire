d1b426f709c38adeaae2f979b11d3be869720f92
sushant <capitalch@gmail.com>
2017-11-16 18:54:39 +0530

    init

--- SHMaestro/options.js
+++ SHMaestro/options.js
@@ -1,40 +1,40 @@
 let options = {
     nodeServerBaseUrl: 'http://localhost:3001',
-    nodeServerPath:'bio',
-    nodeServerGrailPath:'grail',
-    redirection:{
-        "redirect-to-view-consent":"viewConsent1",
-        "redirect-to-survey":"thankyou",
-        "redirect-to-esign-workflow":"question1",
-        "signed-document":"thankyou",
-        "redirect-to-consent-quiz":"quiz",
-        "redirect-to-consent-quiz-complete":"quizPassed",
-        "redirect-to-signed-document":"thankyou",
-        "opt-out-results":"thankyou",
-        "redirect-to-survey-complete":'thankyou'
+    nodeServerPath: 'bio',
+    nodeServerGrailPath: 'grail',
+    redirection: {
+        "redirect-to-view-consent": "viewConsent1",
+        "redirect-to-survey": "thankyou",
+        "redirect-to-esign-workflow": "question1",
+        "signed-document": "thankyou",
+        "redirect-to-consent-quiz": "quiz",
+        "redirect-to-consent-quiz-complete": "quizPassed",
+        "redirect-to-signed-document": "thankyou",
+        "opt-out-results": "thankyou",
+        "redirect-to-survey-complete": 'thankyou'
     },
-    grailRedirection:{
-        "landing-page-completed":"consentCompletedThankYou",
-        "signed-document":"consentDeclined",
-        "redirect-to-survey":"invalidDob"
+    grailRedirection: {
+        "landing-page-completed": "consentCompletedThankYou",
+        "signed-document": "consentDeclined",
+        "redirect-to-survey": "invalidDob"
     }
 };
 
 exports.options = options;
 let pamfOptions = {
     nodeServerBaseUrl: 'http://localhost:4200',
-    nodeServerPath:'pamf',
-    redirection:{
-        "landing-page":function(contact_type,code,email){
-            let ret="";
-            if(code=='12345'){
-                ret="selectType";
-            } else{
-                ret="incorrectTjr";
-                // ret='letterLanding'
+    nodeServerPath: 'pamf',
+    redirection: {
+        "landing-page": function (contact_type, code, email) {
+            let ret = "";
+            if (code == '12345') {
+                ret = "hipLanding";
+            } else {
+                ret = "incorrectTjr";
             }
-            return(ret);
-        }
+            return (ret);
+        },
+        ""
     }
 };
 exports.pamfOptions = pamfOptions;
