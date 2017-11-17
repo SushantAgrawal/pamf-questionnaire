8ce60addea2610618828e3b0ccb35b013905017a
SushantAgrawal <sagarwal@netwoven.com>
2017-11-18 00:23:38 +0530

    minor

--- src/app/unsubscribe-thank-you/unsubscribe-thank-you.component.ts
+++ src/app/unsubscribe-thank-you/unsubscribe-thank-you.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {Router} from '@angular/router';
 import { urlMaps } from '../app.config';
+
 @Component({
   selector: 'app-unsubscribe-thank-you',
   templateUrl: './unsubscribe-thank-you.component.html',
   styleUrls: ['./unsubscribe-thank-you.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class UnsubscribeThankYouComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+  
   exit(){
     window.location.href = urlMaps['pamf:url'];
   }
