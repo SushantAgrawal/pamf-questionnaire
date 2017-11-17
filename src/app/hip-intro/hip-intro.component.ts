337491566b0685a92bf4381920da87dd06792e69
sushant <capitalch@gmail.com>
2017-11-17 17:22:57 +0530

    hip intro

--- src/app/hip-intro/hip-intro.component.ts
+++ src/app/hip-intro/hip-intro.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
+import { AppService } from '../app.service';
+
 @Component({selector: 'app-hip-intro', templateUrl: './hip-intro.component.html', styleUrls: ['./hip-intro.component.scss'], encapsulation: ViewEncapsulation.None})
 export class HipIntroComponent implements OnInit {
 
-  constructor(private router : Router) {}
+  constructor(private router : Router, private appService:AppService) {}
 
   ngOnInit() {}
   next() {
