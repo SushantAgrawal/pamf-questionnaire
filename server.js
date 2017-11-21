eab6e0dd52528feeb7ed1f1bb3ffc7fb3c3e3526
Sushant Agrawal <sagarwal@netwoven.com>
2017-11-21 09:13:54 +0530

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 'use strict';
-
+const path = require('path');
 const express = require('express');
 const app = express();
+
 const argv = require('minimist')(process.argv.slice(2));
 
 // CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods',' GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', ' GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
 });
-
 
 var __publicFolder = __dirname + '/dist';
 app.use(express.static(__publicFolder));
 
 // API
 app.get('/', function (req, res) {
+    res.sendFile(path.join(__publicFolder + '/index.html'));
+});
+app.get('/:page', function (req, res) {
     res.sendFile(path.join(__publicFolder + '/index.html'));
 });
 
