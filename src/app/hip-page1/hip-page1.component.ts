bd6a49052082b84a14e44c228f5d1fb8733c4d18
Sabyasachi Bal <sbal@netwovenindia.com>
2017-11-23 12:30:41 +0530

    update hippage1

--- src/app/hip-page1/hip-page1.component.ts
+++ src/app/hip-page1/hip-page1.component.ts
@@ -1,27 +1,29 @@
-import {Component, OnInit, ViewEncapsulation, ViewChild, ElementRef} from '@angular/core';
-import {Router} from '@angular/router';
-import {hipQuestions} from '../app.questions';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { hipQuestions } from '../app.questions';
 // import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from
 // "@angular/forms"
 
-@Component({selector: 'app-hip-page1', templateUrl: './hip-page1.component.html', styleUrls: ['./hip-page1.component.scss'], encapsulation: ViewEncapsulation.None})
+@Component({ selector: 'app-hip-page1', templateUrl: './hip-page1.component.html', styleUrls: ['./hip-page1.component.scss'], encapsulation: ViewEncapsulation.None })
 export class HipPage1Component implements OnInit {
-  @ViewChild('myForm')myForm : any;
-  start : boolean = true;
-  stairs : string;
-  surface : string;
-  scores : {} = {
-    none: 1,
-    Mild: 2,
-    Moderate: 3,
-    Severe: 4,
-    Extreme: 5
-  };
-  constructor(private router : Router) {
+  @ViewChild('myForm') myForm: any;
+  start: boolean = true;
+  stairs: string;
+  surface: string;
+  scores: any[] = [];
+
+  constructor(private router: Router) {
+    this.scores = [
+      { key: 'none', value: 1 },
+      { key: 'Mild', value: 2 },
+      { key: 'Moderate', value: 3 },
+      { key: 'Severe', value: 4 },
+      { key: 'Extreme', value: 5 }
+    ];
     // this.gender = null;
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
   // change(event, score, value) {   hipQuestions     .responses     .find(x =>
   // x.qx_code === event.srcElement.name)     .answer_text = [];   hipQuestions
   //  .responses     .find(x => x.qx_code === event.srcElement.name)
@@ -29,6 +31,10 @@ export class HipPage1Component implements OnInit {
   // x.qx_code === event.srcElement.name)     .answer_text     .push(value);
   // hipQuestions     .responses     .find(x => x.qx_code ===
   // event.srcElement.name)     .answer_text_score     .push(score); }
+  selectRow(value, val) {
+    if (val == 0) { this.stairs = value.key; }
+    else { this.surface = value.key; }
+  }
   next() {
     this.start = false;
     let thisForm = this.myForm.form;
